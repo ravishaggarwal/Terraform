550af90deb60ae4d02eb6d6a068fa5ed0eb7f765
Ravish Aggarwal <ravish.aggarwal@gmail.com>
2018-06-13 22:57:06 +0530

    Updated and deleted the commented lines

--- Terraform_Exercise5/main.tf
+++ Terraform_Exercise5/main.tf
@@ -4,32 +4,17 @@ terraform {
     encrypt = true
     region = "us-east-1"
     key    = "sampleassumeterraform.tfstate"
-    #profile = "dev"
   }
 }
 
 
 provider "aws" {
-#  access_key = "${var.access_key}"
- # secret_key = "${var.secret_key}"
-  # shared_credentials_file = "%UserProfile%/.aws/credentials"
   region   = "us-east-1"
   assume_role {
     role_arn = "arn:aws:iam::xxxxxxxx:role/xxxxxxx"
     }
 
 }
-
-#  profile  = "dev"
-#}
-
-# Deploy VPC
-# module "vpc" {
- # source       = "../modules/vpc"
-  #cidr_block = "${var.cidr_block}"
-#  env = "${var.env_value}"
-#}
-
 resource "aws_vpc" "Mvpc1" {
 
   cidr_block = "10.0.0.0/24"
